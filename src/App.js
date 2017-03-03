4bf06ae5aa47b4c6a3eab750d141be3bbd91edf0
Jaysig <jasontsigmon@gmail.com>
2017-03-03 17:57:54 -0500

    Starting auth project

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+// import { Field, reduxForm } from 'redux-form';
+// import { connect } from 'react-redux';
+// import * as actions from '../../actions';
+
+class App extends Component {
+
+  render() {
+    return (
+      <View className="">
+        <Text>
+        This is a feature
+         </Text>
+      </View>
+    );
+  }
+}
+// function mapStateToProps(state) {
+//   return { authenticated: state.auth.authenticated };
+// }
+// export default connect(mapStateToProps, actions)(Header)
+
+export default App;
