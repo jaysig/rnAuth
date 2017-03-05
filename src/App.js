f4602b3b5d0b20910200590afa05e76982c34789
Jaysig <jasontsigmon@gmail.com>
2017-03-04 20:47:56 -0500

    Trying to add firebase

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import firebase from 'firebase';
+// import config from 'config';
+import config from '../config/default.json';
+import { Header } from './components/common';
+// const config = require('config');
+// import config as s from '../config/default.json';
 // import { Field, reduxForm } from 'redux-form';
 // import { connect } from 'react-redux';
 // import * as actions from '../../actions';
 
 class App extends Component {
 
+  componentWillMount() {
+    // const fbConfig = config.get('private.firebase');
+    console.log(config.private.firebase);
+    firebase.initializeApp(config.private.firebase);
+  }
   render() {
     return (
       <View className="">
+        <Header headerText="Authentication" />
         <Text>
         This is a feature
          </Text>
