d260cb85708e896eacde9882670d7f0df84036ad
Jaysig <jasontsigmon@gmail.com>
2017-03-04 21:23:55 -0500

    Starting reusable textinput

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,15 +1,23 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, TextInput } from 'react-native';
 import { Button, Card, CardSection } from './common';
 // import { connect } from 'react-redux';
 // import * as actions from '../../actions';
 
 class LoginForm extends Component {
 
+  state = { text: '' };
+
   render() {
     return (
       <Card>
-        <CardSection />
+        <CardSection >
+          <TextInput
+            value={this.state.text}
+            style={{ height: 20, width: 100 }}
+            onChangeText={text => this.setState({ text })}
+          />
+        </CardSection>
         <CardSection />
 
         <CardSection >
